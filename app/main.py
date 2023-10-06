a9b35c4260a927708903abe96b8c7ed5cffeddd7
Can Kinay <me@ckk.im>
2023-10-07 00:01:58 +0200

    send 404 for non root gets

--- app/main.py
+++ app/main.py
@@ -7,8 +7,13 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client, addr = server_socket.accept() # wait for client
-    client.recv(1024) # read request
-    client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+
+    request = client.recv(1024)
+    
+    if request.startswith(b"GET / "):
+        client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        client.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 if __name__ == "__main__":
