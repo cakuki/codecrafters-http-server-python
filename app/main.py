0d95873c55ebf5caf68829757590f1180db35de9
Can Kinay <me@ckk.im>
2023-10-06 23:53:39 +0200

    receive the request and send a response

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    client, addr = server_socket.accept() # wait for client
+    client.recv(1024) # read request
+    client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
