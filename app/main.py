325302cb208fcbb5248a00a142479f7743dcbee5
Can Kinay <me@ckk.im>
2023-10-07 00:14:34 +0200

    add user-agent echo-like path

--- app/main.py
+++ app/main.py
@@ -9,9 +9,17 @@ def main():
     client, addr = server_socket.accept() # wait for client
 
     request = client.recv(1024)
+
+    # get user-agent header from request, which could be in any order
+    user_agent = ""
+    for line in request.decode("utf-8").split("\r\n"):
+        if line.startswith("User-Agent:"):
+            user_agent = line.split(" ", 1)[1]
     
     if request.startswith(b"GET / "):
         client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    elif request.startswith(b"GET /user-agent"):
+        client.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + str(len(user_agent)).encode("utf-8") + b"\r\n\r\n" + user_agent.encode("utf-8"))
     elif request.startswith(b"GET /echo/"):
         url = request.decode("utf-8").split("\r\n")[0].split(" ")[1]
         # split url after second slash, but only once
